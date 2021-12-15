2c7f34428f5745aa814e179e3e80d0565ef4fade
bzpit <891919234@qq.com>
2021-12-15 12:38:21 +0800

    提交

--- answer.py
+++ answer.py
@@ -1,31 +1,32 @@
-def print_hi(question):
-    print(f'Hi, {question}')  # Press Ctrl+F8 to toggle the breakpoint.
-    answer = "I am fine."
-    return answer
+def question(question):
 
-def print_Batman(question):
-    print(f'Hi answer of {question}')
-    answer = 'Hi answer of '+ question +  'is Lorenzo Semple, Jr.'
-    return answer
 
-def print_Daniel(question):
-    answer = 'the answer is yes'
-    return answer
+    if('director' in question and 'Batman' in question):
+        # Who is the director of the Batman movie?
+        print(f'Hi answer of {question}')
+        answer = 'Hi answer of ' + question + 'is Lorenzo Semple, Jr.'
+        return answer
+    elif('Christopher' in question and 'Batman' in question):
+        #Did Christopher Nolan ever work on a Batman movie?
+        answer = 'the answer is yes'
+        return answer
+    elif('Spielberg' in question ):
+        answer = 'Saving Private Ryan'
+        return answer
+    elif('Jurassic Park' in question and 'similar' in question):
+        # I like the Jurassic Park movie; can you recommend any similar movies?
+        answer = 'The Lost World: Jurassic Park , Jurassic Park III, Tyrannosaur, Jurassic World '
+        return answer
+    elif('Catch Me' in question):
+        # What is the name of the lead actor in the movie Catch Me If You
+        answer = 'Karan'
+        return answer
+    else:
+        answer = '无法回答';
+        return answer
 
-#I am a big fan of Steven Spielberg, could you recommend some of his action movies?
-def print_Spielberg(question):
-    answer= 'Saving Private Ryan'
-    return answer
 
-#I like the Jurassic Park movie; can you recommend any similar movies?
-def print_Jurassic(question):
-    answer='The Lost World: Jurassic Park , Jurassic Park III, Tyrannosaur, Jurassic World '
-    return answer
 
-#What is the name of the lead actor in the movie Catch Me If You
-def print_Catch(question):
-    answer='Karan'
-    return answer
 if __name__ == '__main__':
-    answer = print_hi('How are you?')
+    answer = question('Jurassic Park')
     print(answer)
